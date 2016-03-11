056d4db8618b7c504c5aa5982dea08033941e659
Gustav Wellner Bou <gustav.wellnerbou@fotograf.de>
2016-03-11 17:32:13 +0100

    Build sample structure

--- src/Matcher/Voter/CakeRouteVoter.php
+++ src/Matcher/Voter/CakeRouteVoter.php
@@ -4,40 +4,44 @@ namespace GetPhoto\KnpMenuExtension\Matcher\Voter;
 
 use Knp\Menu\ItemInterface;
 use Knp\Menu\Matcher\Voter\VoterInterface;
+use Knp\Menu\MenuItem;
 
-class CakeRouteVoter implements VoterInterface
-{
+class CakeRouteVoter implements VoterInterface {
 	/**
 	 * @var string
 	 */
 	private $route;
 
 	/**
-	 * @param string $regexp
+	 * @param string $route
 	 */
-	public function __construct($route)
-	{
+	public function __construct($route) {
 		$this->route = $route;
 	}
 
-	public function matchItem(ItemInterface $item)
-	{
+	public function matchItem(ItemInterface $item) {
 		if (null === $this->route || null === $item->getUri()) {
 			return null;
 		}
 
+		if($this->route == $item->getUri()) {
+			return true;
+		}
+
+		return null;
+
 		$route = array_filter(explode('/', $this->route));
 		$uri = array_filter(explode('/', $item->getUri()));
 
-		foreach($uri as $key => $part) {
-			if(empty($route[$key])) {
+		foreach ($uri as $key => $part) {
+			if (empty($route[$key])) {
 				return null;
 			}
-			if($part === '*') {
+			if ($part === '*') {
 				continue;
 			}
 
-			if($part !== $route[$key]) {
+			if ($part !== $route[$key]) {
 				return null;
 			}
 		}
